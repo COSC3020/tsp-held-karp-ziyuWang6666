51db575bdadff3566a482b98bdbba1b57dfa7522
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-08 17:44:57 -0700

    Update code.js
    change cache structure to map

--- code.js
+++ code.js
@@ -1,46 +1,41 @@
 function tsp_hk(dm) {
-    let cities = dm.map((item,index) => index)
-    //use built-in sort method
-    cities.sort();
+  let cities = dm.map((item, index) => index);
+  cities.sort();
 
-    if (cities.length == 1) return 0;
+  if (cities.length === 1) return 0;
 
-    let cache = {}
-    let sumDist = null;
-    for (let start in cities) {
-        // remove start = parseInt(start) because right now items are integer.
-        // no need compare Infinity to sumDist since already compared in tsp function
-        // just return value
-        sumDist = tsp(cities, start, dm, cache)
-    }
-    return sumDist;
+  let cache = new Map();
+  let sumDist = Infinity;
+
+  for (let start of cities) {
+    sumDist = tsp(cities, start, dm, cache);
+    // let result = tsp(cities, start, dm, cache);
+    // sumDist = Math.min(sumDist, result);
   }
-  
-  function tsp(cities, start, dm, cache) 
-  {
-      if (cities.length === 1) 
-      {
-          return dm[start][cities[0]];
-      }
-      else
-      {
-          let key = JSON.stringify(cities) + start
-  
-          if (cache[key] === undefined) cache[key] = {}
-      
-          if (cache[key][start] !== undefined) return cache[key][start];
-      
-          let minDist = Infinity;
-      
-          for (let i = 0; i < cities.length; i++) 
-          {
-              var nextStart = cities[i]
-              let newCities = cities.filter(city => city !== start)
-              minDist = Math.min(minDist, dm[start][nextStart] + tsp(newCities, nextStart, dm, cache));
-          }
-  
-          cache[key][start] = minDist;
-          return minDist;
-      }
+
+  return sumDist;
 }
-// get source from tsp-held-karp-rzafft1 and revised a little bit
+
+function tsp(cities, start, dm, cache) {
+  if (cities.length === 1) {
+    return dm[start][cities[0]];
+  } else {
+    
+    let key = JSON.stringify(cities) + start;
+
+    if(cache.has(key)) {
+      return cache.get(key);
+    }
+    let minDist = Infinity;
+
+    for (let i = 0; i < cities.length; i++) {
+      let nextStart = cities[i];
+      let newCities = cities.filter((city) => city !== start);
+      minDist = Math.min(minDist, dm[start][nextStart] + tsp(newCities, nextStart, dm, cache));
+    }
+
+    cache.set(key, minDist);
+    return minDist;
+  }
+}
+// get help from TA
